cffcaee9301b90ff5d69a9b86b0f198ad78e4676
Mariia Kravchenko <mariia.kravchenko2204@gmail.com>
2024-10-16 20:02:59 -0400

    init: submit solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.max + this.min)/2)
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
+        return Math.ceil((this.min + this.middle)/2) - 1;
     }
 
     greater() {
+        this.min = this.middle;
+        return Math.ceil((this.max + this.middle)/2) + 1;
 
     }
 }
